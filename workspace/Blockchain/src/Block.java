0caf5f64bbd1745e32d573b33750e378f9d06182
Evan <evan.rypel@gmail.com>
2018-08-22 12:01:39 -0700

    Added method to create Merkle Tree

--- workspace/Blockchain/src/Block.java
+++ workspace/Blockchain/src/Block.java
@@ -5,51 +5,50 @@ public class Block {
 	public String hash; // digital signature
 	public String previousHash; // the previous block's hash
 	public String merkleRoot;
-	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data 
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); // our data
 	private long timeStamp; // as a number of milliseconds since 1/1/1970
 	private int nonce;
-	
-	
+
 	// Block constructor
 	public Block(String previousHash) {
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
-		
+
 		// make sure to do this after setting the other values
 		this.hash = calculateHash();
 	}
 
 	public String calculateHash() {
-		String calculatedHash = StringUtil.applySha256(
-				previousHash + 
-				Long.toString(timeStamp) +
-				Integer.toString(nonce) +
-				merkleRoot);
+		String calculatedHash = StringUtil
+				.applySha256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot);
 		return calculatedHash;
 	}
-	
+
 	/**
-	 * Low difficulty of 1 or 2 can be solved nearly instantly on most computers
-	 * 4-6 is good for testing.
+	 * Low difficulty of 1 or 2 can be solved nearly instantly on most computers 4-6
+	 * is good for testing.
+	 * 
 	 * @param difficulty
 	 */
 	public void mineBlock(int difficulty) {
-		merkleRoot = StringUtil.getMerkleRoot(transactions);
+		ArrayList<String> merkleTree = StringUtil.getMerkleTree(transactions);
+		merkleRoot = StringUtil.getMerkleRoot(merkleTree);
 		// create a String with difficulty * "0"
 		String target = new String(new char[difficulty]).replace('\0', '0');
-		while(!hash.substring(0, difficulty).equals(target)) {
+		while (!hash.substring(0, difficulty).equals(target)) {
 			nonce++;
 			hash = calculateHash();
 		}
 		System.out.println("Block mined!! : " + hash);
 	}
-	
-	//add transactions to this block
+
+	// add transactions to this block
 	public boolean addTransaction(Transaction transaction) {
-		//process transaction and check if valid. Ignore if this is the genesis block
-		if(transaction == null) return false;
-		if((previousHash != "0")) {
-			if(transaction.processTransaction() != true) {
+		// process transaction and check if valid. Ignore if this is the genesis block
+		if (transaction == null)
+			return false;
+		if ((previousHash != "0")) {
+			if (transaction.processTransaction() != true) {
 				System.out.println("#Transaction failed to process. Discarded.");
 				return false;
 			}
--- workspace/Blockchain/src/StringUtil.java
+++ workspace/Blockchain/src/StringUtil.java
@@ -11,46 +11,42 @@ import java.util.Base64;
 public class StringUtil {
 	public final static String SHA_256 = "SHA-256";
 	public final static String UTF_8 = "UTF-8";
-	
+
 	/**
 	 * 
-	 * This method takes a string and applies SHA256 encryption
-	 * algorithm to it. it returns a generated signature as a String
+	 * This method takes a string and applies SHA256 encryption algorithm to it. it
+	 * returns a generated signature as a String
 	 * 
-	 * @param input: data to be encrypted with SHA256
+	 * @param input:
+	 *            data to be encrypted with SHA256
 	 * @return a SHA256 encrypted hexadecimal String
 	 */
-	public static String applySha256(String input)
-	{
-		try
-		{
+	public static String applySha256(String input) {
+		try {
 			MessageDigest digest = MessageDigest.getInstance(SHA_256);
 			// encrypts input with sha256
 			byte[] hash = digest.digest(input.getBytes(UTF_8));
-			
-			//we want a hexadecimal string for the hash
+
+			// we want a hexadecimal string for the hash
 			StringBuffer hexString = new StringBuffer();
-			for(int i = 0; i < hash.length; i++)
-			{
+			for (int i = 0; i < hash.length; i++) {
 				// mask the variable so it leaves only the value
 				// in the last 8 bits. Ignore the rest of the bits
 				String hex = Integer.toHexString(0xff & hash[i]);
-				if(hex.length() == 1)
-				{
+				if (hex.length() == 1) {
 					hexString.append('0');
 				}
 				hexString.append(hex);
 			}
 			return hexString.toString();
 		}
-		//NoSuchAlgorithmException or UnsupportedEncodingException
-		catch (Exception e)
-		{
+		// NoSuchAlgorithmException or UnsupportedEncodingException
+		catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	//Applies ECDSA Signature and returns the result (as bytes)
+
+	// Applies ECDSA Signature and returns the result (as bytes)
 	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
 		Signature dsa;
 		byte[] output = new byte[0];
@@ -61,12 +57,12 @@ public class StringUtil {
 			dsa.update(strByte);
 			byte[] realSig = dsa.sign();
 			output = realSig;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 		return output;
 	}
-	
+
 	// Verifies a String signature
 	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
 		try {
@@ -74,34 +70,49 @@ public class StringUtil {
 			ecdsaVerify.initVerify(publicKey);
 			ecdsaVerify.update(data.getBytes());
 			return ecdsaVerify.verify(signature);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	public static String getStringFromKey(Key key) {
-		
+
 		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
-	
-	//Tacks in array of transactions and returns a faux merkle root
-	//TODO implement real merkle root
-	public static String getMerkleRoot(ArrayList<Transaction> transactions) {
-		int count = transactions.size();
-		ArrayList<String> previousTreeLayer = new ArrayList<String>();
-		for(Transaction t : transactions) {
-			previousTreeLayer.add(t.transactionId);
-		}
-		ArrayList<String> treeLayer = previousTreeLayer;
-		while(count > 1) {
-			treeLayer = new ArrayList<String>();
-			for(int i = 1; i < previousTreeLayer.size(); i++) {
-				treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
-			}
-			count = treeLayer.size();
-			previousTreeLayer = treeLayer;
-		}
-		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+
+	public static String getMerkleRoot(ArrayList<String> merkleTree) {
+		String merkleRoot = (merkleTree.size() == 1) ? merkleTree.get(0) : "";
 		return merkleRoot;
+	}
+
+	// method to create a Merkle Tree out of the transaction list
+	// Tacks in array of transactions and returns a merkle tree
+	public static ArrayList<String> getMerkleTree(ArrayList<Transaction> transactions) {
+		ArrayList<String> tree = new ArrayList<>();
+		// add all the hashes of the transactions to the tree
+		for (Transaction t : transactions) {
+			tree.add(t.transactionId); // add the hash
+		}
+
+		// Offset in the list where the currently processed level starts
+		int levelOffset = 0;
+
+		// Step thru each level and stop when we reach the root (levelSize == 1)
+		for (int levelSize = transactions.size(); levelSize > 1; levelSize = (levelSize + 1) / 2) {
+			// for each pair of nodes on the level:
+			for (int left = 0; left < levelSize; left += 2) {
+				/*
+				 * The right hand node can be the same as the left hand in the case that we
+				 * don't have enough transactions.
+				 */
+				int right = Math.min(left + 1, levelSize - 1);
+				String leftTransaction = tree.get(levelOffset + left);
+				String rightTransaction = tree.get(levelOffset + right);
+				tree.add(StringUtil.applySha256(leftTransaction + rightTransaction));
+			}
+			// move to next level
+			levelOffset += levelSize;
+		}
+		return tree;
 	}
 }
